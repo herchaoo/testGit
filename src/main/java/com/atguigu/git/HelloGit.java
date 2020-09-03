25af3a0b8ba039eedc482e6ad1c7b115959af438
herchaoo <herchaoo@qq.com>
2020-09-03 15:32:25 +0800

    删除更新1

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -3,7 +3,6 @@ package com.atguigu.git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git.");
-		System.out.println("更新1");
 		System.out.println("创建分支dev后更新HelloGit.java");
 		System.out.println("master添加");
 		System.out.println("dev添加");
